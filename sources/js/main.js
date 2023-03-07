8d193a1e813b8c9fe4b5770b74a2a1803f580ea2
JulianeMonteiro <juliane.a.monteiro@hotmail.com>
2023-03-07 18:16:48 -0300

    Projeto Finalizado

--- sources/js/main.js
+++ sources/js/main.js
@@ -1,4 +1,4 @@
-const panels = document.querySelectorAll(".panel");
+// NAV
 
 const nav = document.querySelector(".nav");
 window.addEventListener("scroll", fixNav);
@@ -10,6 +10,10 @@ function fixNav() {
     nav.classList.remove("active");
   }
 }
+
+// PANELS
+
+const panels = document.querySelectorAll(".panel");
 
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
@@ -23,3 +27,38 @@ function removeActiveClasses() {
     panel.classList.remove("active");
   });
 }
+
+// DATE
+
+function pad(valor) {
+  return valor.toString().padStart(2, "0");
+}
+
+function formata(data) {
+  return (
+    data.getFullYear() +
+    "-" +
+    pad(data.getMonth() + 1) +
+    "-" +
+    pad(data.getDate()) +
+    "T" +
+    pad(data.getHours()) +
+    ":" +
+    pad(data.getMinutes())
+  );
+}
+
+var campo = document.querySelectorAll(".agend");
+var hoje = formata(new Date());
+
+document.addEventListener("DOMContentLoaded", function () {
+  campo.min = hoje;
+});
+
+campo.addEventListener("invalid", function (e) {
+  e.target.setCustomValidity(`A data não pode ser anterior a ${hoje}`);
+});
+campo.addEventListener("input", function (e) {
+  e.target.setCustomValidity("");
+  e.target.checkValidity();
+});
